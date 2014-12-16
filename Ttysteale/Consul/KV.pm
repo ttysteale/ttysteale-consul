444335d596defa99bdb0c9c4729665bec4ee8386
Sam Teale <sam_teale@trendmicro.co.uk>
2014-12-16 15:22:39 +0000

    Update KV

--- Ttysteale/Consul/KV.pm
+++ Ttysteale/Consul/KV.pm
@@ -7,13 +7,14 @@ use JSON;
 use Data::Dumper;
 
 sub new {
-
     my %args = @_;
+    my %ua_args = ();
+    my $self = {};
+
     $self->{consul_server} = $args{consul_server} || 'localhost';
     $self->{proto} = $args{proto} || 'http';
     $self->{consul_port} = $args{port} || 8500;
 
-    my %ua_args = ();
     $ua_args{ssl_opts} = $args{ssl_opts} if $args{ssl_opts};
     $self->{ua} = LWP::UserAgent->new(%ua_args);
 
@@ -21,4 +22,27 @@ sub new {
 
 }
 
+sub _send_req {
+    my $self = shift;
+    my $req  = shift;
+    my %req_params = @_;
+    
+    my $ret = $self->{ua}->request($req, %req_params);
+    
+    if (not defined($ret->is_success)) { return 'http request failed with ' . 
+$ret->status_line; }
 
+    return 0;
+}
+
+sub KVdelete {
+
+    
+
+}
+
+sub KVget {}
+
+sub KVput {}
+
+1;
